80a26958283f7e91315ec3ba29de790dbd0e4d72
kiselmen <kiselmen@rambler.ru>
2023-01-03 22:35:14 +0300

    feat: catalog + filter panel

--- front/src/components/catalog/filter-panel.vue
+++ front/src/components/catalog/filter-panel.vue
@@ -1,87 +1,40 @@
 <template>
     <div class="filter__block">
-        <div class="filter__box">
-            <div class="filter__title icon-arrow-up">Тип оборудования</div>
-
-            <div class="filter__checkbox-list">
-            <div class="filter__checkbox__item">
-                <div class="checkbox-default">
-                <label class="checkbox__label">
-                    <input type="checkbox" name="" class="" value="">
-                    <div class="checkbox"></div>
-                    <div class="filter__text">
-                    <span class="">Кабели и провода</span>
+        <div class="filter__box" 
+            v-for   = "item in TOP_CATEGORIES"
+            :key    = "item.id"
+            @click  = "changeCategory(item.id, $event)"
+        >
+            <div class="filter__title icon-arrow-up">{{item.name}}</div>
+            <div class="filter__block"  v-if = "item.id === TOP_CATEGORIES_ITEM_ACTIVE">
+                <div class="filter__box" 
+                    v-for   = "sub in SUB_CATEGORIES"
+                    :key    = "sub.id"
+                    @click  = "subCategoryClick(sub.id, $event)"
+                >
+                    <div class="filter__title icon-arrow-up">{{sub.name}}</div>
+                    <div class="filter__checkbox-list"  v-if = "sub.id === SUB_CATEGORIES_ITEM_ACTIVE">
+                        <div class="filter__checkbox__item"
+                            v-for = "subItem in sub.subItems"
+                            :key  = "subItem.id"
+                            @click = "subItemCategoryClick(subItem.id, $event)"
+                        >
+                            <div class="checkbox-default">
+                                <label class="checkbox__label">
+                                    <input type="checkbox" name="" class="" value="">
+                                    <div class="checkbox"></div>
+                                    <div class="filter__text">
+                                        <span class="">{{subItem.name}}</span>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
                     </div>
-                </label>
                 </div>
             </div>
-            <div class="filter__checkbox__item">
-                <div class="checkbox-default">
-                <label class="checkbox__label">
-                    <input type="checkbox" name="" class="" value="">
-                    <div class="checkbox"></div>
-                    <div class="filter__text">
-                    <span class="">Сетевое оборудование</span>
-                    </div>
-                </label>
-                </div>
-            </div>
-            <div class="filter__checkbox__item">
-                <div class="checkbox-default">
-                <label class="checkbox__label">
-                    <input type="checkbox" name="" class="" value="">
-                    <div class="checkbox"></div>
-                    <div class="filter__text">
-                    <span class="">Группа 3</span>
-                    </div>
-                </label>
-                </div>
-            </div>
-            </div>
-
 
         </div>
-        <div class="filter__box">
-            <div class="filter__title icon-arrow-up">Категория</div>
 
-            <div class="filter__checkbox-list">
-            <div class="filter__checkbox__item">
-                <div class="checkbox-default">
-                <label class="checkbox__label">
-                    <input type="checkbox" name="" class="" value="">
-                    <div class="checkbox"></div>
-                    <div class="filter__text">
-                    <span class="">Плинты размыкаетмые</span>
-                    </div>
-                </label>
-                </div>
-            </div>
-            <div class="filter__checkbox__item">
-                <div class="checkbox-default">
-                <label class="checkbox__label">
-                    <input type="checkbox" name="" class="" value="">
-                    <div class="checkbox"></div>
-                    <div class="filter__text">
-                    <span class="">Коннекторы, разъемы, соединители</span>
-                    </div>
-                </label>
-                </div>
-            </div>
-            <div class="filter__checkbox__item">
-                <div class="checkbox-default">
-                <label class="checkbox__label">
-                    <input type="checkbox" name="" class="" value="">
-                    <div class="checkbox"></div>
-                    <div class="filter__text">
-                    <span class="">Категория 3</span>
-                    </div>
-                </label>
-                </div>
-            </div>
-            </div>
-
-
-        </div>
         <div class="filter__box">
             <div class="filter__title icon-arrow-up">Показывать:</div>
 
@@ -129,19 +82,45 @@
 
 <script>
 
-import {mapActions, mapGetters} from 'vuex'
+import {mapMutations, mapGetters} from 'vuex'
 
 export default {
     name: 'FilterPanel',
 
     computed: {
-    // ...mapGetters("header", ["IS_CATALOG_OPEN", "TOP_CATEGORIES"]),
-    // ...mapGetters("order", ["ORDER_COUNT"]),
+        ...mapGetters("header", ["TOP_CATEGORIES_ITEM_ACTIVE", "SUB_CATEGORIES_ITEM_ACTIVE", "TOP_CATEGORIES", "SUB_CATEGORIES"]),
+    },
+
+    watch: {
+
     },
 
     methods:{
+        ...mapMutations("header", ["SET_CURRENT_TOP_CATEGORY", "SET_CURRENT_SUB_CATEGORY"]),
+        changeCategory(newActive, event){
+            if (this.TOP_CATEGORIES_ITEM_ACTIVE === newActive) {
+                this.SET_CURRENT_TOP_CATEGORY(null);
+            } else {
+                this.SET_CURRENT_TOP_CATEGORY(newActive);
+            }
+        },
+        subCategoryClick(id, event){
+            event.stopImmediatePropagation();
+            event.preventDefault();
+            console.log('SUB', id);
+            if (this.SUB_CATEGORIES_ITEM_ACTIVE === id) {
+                this.SET_CURRENT_SUB_CATEGORY(null);
+            } else {
+                this.SET_CURRENT_SUB_CATEGORY(id);
+            }
+        },
+        subItemCategoryClick(id, event){
+            event.stopImmediatePropagation();
+            event.preventDefault();
+            console.log('кликнули по итему подкатегории ', id, event);
+        },
 
-    }    
+    },
 }
 </script>
 
--- front/src/views/Catalog.vue
+++ front/src/views/Catalog.vue
@@ -24,90 +24,9 @@
                   <div class="topfilter__box-view icon-catalog-table"></div>
                   <div class="topfilter__row-view icon-catalog-row active"></div>
                 </div>
-
-
               </div>
               <div class="content-block__list">
-                <div class="content-block__item product-row">
-                  <div class="product">
-                    <div class="product__tag">Хит</div>
-                    <a class="product__img" href="">
-                      <img class="" src="../assets/image44.png" alt="">
-                    </a>
-                    <div class="product__info">
-                      <div class="product__status icon-done-color _label mb-20">В наличии</div>
-                      <div class="product__title">
-                        <a  href="">Коммуникационный кабель</a>
-                      </div>
-                      <div class="product__uptitle">
-                        <a  href="">UTP cat.5e (патч-панель) 19″</a>
-                      </div>
-                      <div class="product__count flex-center">
-                        <span class="_label">Количество:</span>
-                        <span class="icon-minus"></span>
-                        <input class="product__input" type="text">
-                        <span class="icon-plus"></span>
-                      </div>
-
-
-
-
-                    </div>
-                    <div class="product__action">
-                      <div class="product__article  _label mb-20">Артикул: <span>331003</span></div>
-                      <div class="product__price">
-                        <span>70</span>BYN
-                        <span>/шт</span>
-                      </div>
-                      <div class="notice">* Цена указана с учетом НДС.</div>
-                      <div class="product__btn flex-center">
-                        <div class="product__wishlist icon-favorite"></div>
-                        <div class="btn black">В корзину</div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <div class="content-block__item product-row">
-                  <div class="product">
-                    <div class="product__tag">Хит</div>
-                    <a class="product__img" href="">
-                      <img class="" src="../assets/image44.png" alt="">
-                    </a>
-                    <div class="product__info">
-                      <div class="product__status icon-done-color _label mb-20">В наличии</div>
-                      <div class="product__title">
-                        <a  href="">Коммуникационный кабель</a>
-                      </div>
-                      <div class="product__uptitle">
-                        <a  href="">UTP cat.5e (патч-панель) 19″</a>
-                      </div>
-                      <div class="product__count flex-center">
-                        <span class="_label">Количество:</span>
-                        <span class="icon-minus"></span>
-                        <input class="product__input" type="text">
-                        <span class="icon-plus"></span>
-                      </div>
-
-
-
-
-                    </div>
-                    <div class="product__action">
-                      <div class="product__article  _label mb-20">Артикул: <span>331003</span></div>
-                      <div class="product__price">
-                        <span>70</span>BYN
-                        <span>/шт</span>
-                      </div>
-                      <div class="notice">* Цена указана с учетом НДС.</div>
-                      <div class="product__btn flex-center">
-                        <div class="product__wishlist icon-favorite"></div>
-                        <div class="btn black">В корзину</div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
+                <CatalogItem />
               </div>
 
             </div>
@@ -120,14 +39,15 @@
 
 <script>
 
-  import FilterPanel from '@/components/catalog/filter-panel.vue'
+  import FilterPanel from '@/components/catalog/filter-panel.vue';
+  import CatalogItem from '@/components/catalog/catalog-item.vue';
 
   export default {
     name: 'Catalog',
 
     components:
     {
-      FilterPanel,
+      FilterPanel, CatalogItem
     },
 
   }
@@ -219,9 +139,6 @@
       }
     }
   }
-  &__item{
-    margin-bottom: 16px;
-  }
 
   &__topfilter{
     display: flex;
@@ -253,176 +170,5 @@
   }
 
 }
-
-.product{
-    display: flex;
-    position: relative;
-    background: #FFFFFF;
-    border: 2px solid #EEEEEE;
-    box-sizing: border-box;
-    border-radius: 8px;
-    padding: 20px 22px 20px 22px;
-
-  ._label{
-    font-size: 12px;
-    line-height: 20px;
-  }
-
-    &__img {
-      width: 100%;
-      flex-basis: 30%;
-      img{
-        max-width: 100%;
-      }
-    }
-  &__info{
-    flex-basis: 45%;
-    padding: 0 10px;
-
-  }
-
-  &__action{
-    flex-basis: 25%;
-    padding: 0 10px;
-  }
-
-    &__tag{
-      background: #7700AF;
-      border-radius: 2px;
-      padding: 2px 11px;
-      position: absolute;
-      font-weight: 400;
-      font-size: 12px;
-      left:20px;
-      top:20px;
-      color: #fff;
-    }
-    &__wishlist{
-
-      &.added {
-        fill: #ff6f60;
-        path {
-          stroke: #ff6f60;
-        }
-      }
-
-      cursor: pointer;
-      fill: none;
-      margin-right: 10px;
-    }
-  &__price{
-    font-size: 20px;
-    margin-bottom: 10px;
-    text-align: right;
-    span:nth-child(1){
-      font-weight: 500;
-      margin-right: 5px;
-    }
-  }
-
-
-    &__row {
-      justify-content: space-between;
-      &:nth-child(1){
-        margin-bottom: 3px;
-      }
-      &:nth-child(2){
-        margin-bottom: 15px;
-      }
-    }
-
-    &__buy {
-      &:before {
-        cursor: pointer;
-        padding: 10px 10px;
-      }
-      &:hover{
-        background: #4275D8;
-        border-radius: 6px;
-        color: #fff;
-      }
-
-
-    }
-
-    .notice{
-      font-weight: 300;
-      font-size: 10px;
-      opacity: 0.5;
-      text-align: right;
-    }
-
-
-
-    .current_price {
-      font-weight: 500;
-      font-size: 20px;
-      line-height: 24px;
-      span{
-        font-weight: 300;
-      }
-
-    }
-
-    &__title {
-      margin-bottom: 10px;
-      a{
-        font-weight: 500;
-        font-size: 15px;
-
-        color: #423E48;
-      }
-
-    }
-
-    &__uptitle {
-      a{
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 130%;
-        color: #423E48;
-      }
-
-    }
-  &__status{
-    &:before{
-      margin-right: 10px;
-    }
-  }
-  &__article{
-    text-align: right;
-  }
-
-  &__count{
-    margin: 24px 0;
-    span:nth-child(1){
-      margin-right: 10px;
-    }
-
-    .icon-plus, .icon-minus{
-      cursor: pointer;
-    }
-  }
-
-  &__input{
-    width: 40px;
-    height: 40px;
-    padding: 9px 8px;
-    background: rgba(66, 62, 72, 0.07);
-    border-radius: 2px;
-    border: none;
-    margin: 0 10px;
-  }
-
-  &__btn{
-    margin: 24px 0 ;
-
-
-  }
-
-
-
-}
-
 
 </style>
