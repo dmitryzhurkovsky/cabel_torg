00ecdfdb6db6f6594e27d6587f784e88cc5a3ec8
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2023-01-25 15:19:06 +0100

    Change style for popup

--- front/src/components/notifications/pop-up.vue
+++ front/src/components/notifications/pop-up.vue
@@ -3,13 +3,11 @@
       <div 
         :class="[IS_POP_UP_OPEN === true ? 'popup__wrapper': 'popup__wrapper disabled']"
       >
-        <div class="popup__header">
-            <div class="icon-close left-menu" @click="closePopUp(false)"></div>
-            <div class="right-menu">
-                <TopMenuActions @click="closePopUp(false)" />
-            </div>
-        </div>
+
+
+
         <div class="popup__body">
+            <div class="icon-close popup__close" @click="closePopUp(false)"></div>
             <div class="popoup__content">
                 <div class="popup__message">Тут содежимое PopUp</div>
             </div>
@@ -54,33 +52,37 @@
     &__wrapper {
         display: flex;
         flex-direction: column;
-        justify-content: flex-start;
+        justify-content: center;
+        align-items: center;
         position: absolute;
         left: 0;
         top: 0;
-        height: 100%;
+        //height: 100%;
         // max-height:100vh;
         width: 100%;
-        overflow: hidden;
-        background-color: #FFFFFF;
+        background: linear-gradient(180deg, rgba(66, 62, 72, 0.2) 0%, rgba(66, 62, 72, 0) 100%);
+        backdrop-filter: blur(2px);
         z-index: 95;
+        height: 100vh;
+        overflow-y: hidden;
     }
 
-    &__header {
-        // position: absolute;
-        height: 60px;
-        // width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 20px;
-        border-bottom: 1px solid #423E48 ;
+    &__close {
+      position: absolute;
+      top:0;
+      right: -30px;
     }
     &__body {
+        position: relative;
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100%;
+        width: 400px;
+        height: 300px;
+        background: #fff;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.08);
+        border-radius: 16px;
+
     }
 
 }
