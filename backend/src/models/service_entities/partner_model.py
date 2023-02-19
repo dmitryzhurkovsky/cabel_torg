a4097734f486d4f8c4328947b3b002339c420f12
dmitryzhurkovsky <dmitryzhurkovsky@gmail.com>
2023-02-19 18:02:36 +0300

    Add vendor info and addresses models; Add service__ prefix for service models

--- backend/src/models/service_entities/partner_model.py
+++ backend/src/models/service_entities/partner_model.py
@@ -4,7 +4,7 @@ from src.core.db.db import Base
 
 
 class Partner(Base):
-    __tablename__ = 'partners'
+    __tablename__ = 'service__partners'
 
     id = Column(Integer, index=True, primary_key=True)
     image = Column(String, nullable=True)
