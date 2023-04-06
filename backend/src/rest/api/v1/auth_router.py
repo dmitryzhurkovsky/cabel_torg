e0373d9f2f1eb071819054ab8a1908b0c83caf78
dmitryzhurkovsky <dmitryzhurkovsky@gmail.com>
2023-04-06 19:24:20 +0300

    Change: Add refresh token to response

--- backend/src/rest/api/v1/auth_router.py
+++ backend/src/rest/api/v1/auth_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Cookie
+from fastapi import APIRouter, Depends
 from fastapi import status, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,7 +6,7 @@ from starlette.responses import JSONResponse
 
 from src.core.db.db import get_session
 from src.core.enums import SessionStatusEnum
-from src.rest.schemas.auth_schema import AuthenticationResponseSchema
+from src.rest.schemas.auth_schema import AuthenticationResponseSchema, UpdateRefreshTokenSchema
 from src.services.auth_service import AuthService
 from src.services.user_service import UserService
 
@@ -15,22 +15,28 @@ auth_router = APIRouter(tags=['auth'])
 
 @auth_router.post('/token', response_model=AuthenticationResponseSchema, status_code=status.HTTP_201_CREATED)
 async def sign_in(
-        response: Response,
         user_form: OAuth2PasswordRequestForm = Depends(),
         session: AsyncSession = Depends(get_session),
 ) -> AuthenticationResponseSchema:
     access_token, refresh_token = await AuthService.get_access_and_refresh_tokens_by_email_and_password(
         session=session, user_form=user_form
     )
-    response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
+    return AuthenticationResponseSchema(
+        access_token=access_token,
+        refresh_token=refresh_token
+    )
 
-    return AuthenticationResponseSchema(access_token=access_token)
 
-
-@auth_router.post('/refresh', status_code=status.HTTP_201_CREATED)
-async def obtain_pair_of_tokens_by_refresh_token(refresh_token: str = Cookie(None)):
-    await AuthService.get_new_access_and_refresh_tokens_using_old_refresh_token(
-        old_refresh_token=refresh_token
+@auth_router.post('/refresh', response_model=AuthenticationResponseSchema, status_code=status.HTTP_201_CREATED)
+async def obtain_pair_of_tokens_by_refresh_token(
+        token_info: UpdateRefreshTokenSchema
+) -> AuthenticationResponseSchema:
+    access_token, refresh_token = await AuthService.get_new_access_and_refresh_tokens_using_old_refresh_token(
+        old_refresh_token=token_info.refresh_token
+    )
+    return AuthenticationResponseSchema(
+        access_token=access_token,
+        refresh_token=refresh_token
     )
 
 
