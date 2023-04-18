a248f85315c061467e57955c22ad14db43c84316
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2023-04-18 15:41:33 +0200

    Update product cart

--- front/src/views/Card_product_grid.vue
+++ front/src/views/Card_product_grid.vue
@@ -3,99 +3,87 @@
     <div class="product__wrapper">
       <div class="product__content _container">
         <div class="product__body">
-
-          <div class="grid">
-            <div class="grid__item" tabindex="1">
+          <div class="product__box flex-center">
               <div class="product__main-img">
-                <CardImage :images=cartItemData.images />
+                  <CardImage :images=cartItemData.images />
               </div>
-            </div>
-            <div class="grid__item" tabindex="2">
-              <div class="product__add-img add-img">
-              </div>
-            </div>
-            <div class="grid__item" tabindex="3">
-              <div class="desc-product__title"> {{ cartItemData.name }}</div>
-            </div>
-            <div class="grid__item" tabindex="4">
-              <div class="desc-product__article  _label">Артикул: <span>{{ cartItemData.vendor_code }}</span></div>
-            </div>
-            <div class="grid__item" tabindex="5">
-            </div>
-            <div class="grid__item" tabindex="6">
-<!--              <div class="status-row__link icon-share"><span>Поделиться</span></div>-->
-            </div>
-            <div class="grid__item" tabindex="7">
-              <div class="desc-product__status icon-done-color _label" v-if = "cartItemData.status === 'A'">В наличии</div>
-              <div class="desc-product__status icon-on-the-way " v-if = "cartItemData.status === 'W'">В пути на склад</div>
-              <div class="desc-product__status if_status_on_the_way _label" v-if = "cartItemData.status === 'W'">Доставим в течение 14 дней</div>
-              <div class="desc-product__status icon-out-of-stock _label" v-if = "cartItemData.status === 'O'">Нет в наличии</div>
-            </div>
-            <div class="grid__item" tabindex="8">
-              <div class="price-product__block">
-                <div class="_label">Ваша цена:</div>
-                <div class="current_price">
-                  <span 
-                    v-if="cartItemData.price_with_discount_and_tax && cartItemData.price_with_tax !== cartItemData.price_with_discount_and_tax"
-                    class="old_price"
-                  >{{ cartItemData.price_with_tax }}</span>
-                  <span>
-                    {{ cartItemData.price_with_discount_and_tax && cartItemData.price_with_discount_and_tax !== cartItemData.price_with_tax 
-                          ? cartItemData.price_with_discount_and_tax 
-                          : cartItemData.price_with_tax 
-                    }}
-                  </span>BYN
-                  <!-- cartItemData.price  -->
-                  <span>/{{ cartItemData.base_unit.full_name }}</span>
-                </div>
-              </div>
-            </div>
-            <div class="grid__item" tabindex="9">
-              <div 
-                  @click.stop="onWishClick()"
-                  :class="[isWish === false ? 'product__favorite icon-favorite' : 'product__favorite icon-favorite-choosed']" 
-              >
-              </div>
-            </div>
-            <div class="grid__item" tabindex="10">
-              <div class="price-product__block">
-                <div class="retail_price">
-                  <div>Первоначальная цена: </div>
-                  <div>
-                    <span class="price__value"> {{ cartItemData.price_with_tax }}</span>
-                      BYN
-                    <span>/шт</span>
+              <div class="product__info">
+                  <div class="desc-product__title"> {{ cartItemData.name }}</div>
+                  <div class="desc-product__article  _label">Артикул: <span>{{ cartItemData.vendor_code }}</span></div>
+                  <div class="desc-product__status icon-done-color _label" v-if = "cartItemData.status === 'A'">В наличии</div>
+                  <div class="desc-product__status icon-on-the-way " v-if = "cartItemData.status === 'W'">В пути на склад</div>
+                  <div class="desc-product__status if_status_on_the_way _label" v-if = "cartItemData.status === 'W'">Доставим в течение 14 дней</div>
+                  <div class="desc-product__status icon-out-of-stock _label" v-if = "cartItemData.status === 'O'">Нет в наличии</div>
+
+                  <div class="desc-product__count">
+                      <span class="_label">Количество</span>
+                      <span class="icon-minus" @click.stop="minusQuantityLocal"></span>
+                      <input class="desc-product__input"  type="number" v-model="quantityLocal" @click.stop="" @input="checkQuantityLocal">
+                      <span class="icon-plus" @click.stop="plusQuantityLocal"></span>
                   </div>
 
-                </div>
-                <div class="opt_price">
-                  <div>Цена со скидкой: </div>
-                  <div>
-                    <span class="price__value">{{ 
-                        cartItemData.price_with_tax }}</span>BYN
-                    <span>/шт</span>
-                  </div>
+                  <div class="desc-product__price">
+                      <div class="price__left">
+                          <div class="_label">Ваша цена:</div>
+                          <div class="current_price">
+                          <span v-if="cartItemData.price_with_discount_and_tax && cartItemData.price_with_tax !== cartItemData.price_with_discount_and_tax"
+                                class="old_price"
+                          >{{ cartItemData.price_with_tax }}
+                          </span>
+                              <span>
+                              {{ cartItemData.price_with_discount_and_tax && cartItemData.price_with_discount_and_tax !== cartItemData.price_with_tax
+                                  ? cartItemData.price_with_discount_and_tax
+                                  : cartItemData.price_with_tax
+                                  }}
+                          </span>BYN
+                              <!-- cartItemData.price  -->
+                              <span class="current_price_item">/{{ cartItemData.base_unit.full_name }}</span>
+                          </div>
+                      </div>
+                      <div class="price__right">
+                          <div class="retail_price">
+                            <div>Первоначальная цена: </div>
+                            <div>
+                                <span class="price__value"> {{ cartItemData.price_with_tax }}</span>
+                                BYN
+                                <span>/шт</span>
+                            </div>
 
-                </div>
-              </div>
-            </div>
-            <div class="grid__item" tabindex="11">
-              <div v-if="quantity !== 0" class="btn empty_black" @click.stop="onOperationWithCartItem(cartItemData, 'set')">В корзине</div>
-              <div v-if="quantity === 0 && cartItemData.status !== 'O'" class="btn black" @click.stop="onOperationWithCartItem(cartItemData, 'set')">В корзину</div>
-              <div v-if="quantity === 0 && cartItemData.status === 'O'" class="btn empty_black popup-btn" @click.stop="onCreatePopUp(true, cartItemData.id)">Узнать о поступлении</div>
-            </div>
-            <div class="grid__item" tabindex="12">
-              <div class="desc-product__count">
-                <span class="_label">Количество</span>
-                <span class="icon-minus" @click.stop="minusQuantityLocal"></span>
-                <input class="desc-product__input"  type="number" v-model="quantityLocal" @click.stop="" @input="checkQuantityLocal">
-                <span class="icon-plus" @click.stop="plusQuantityLocal"></span>
-              </div>
-            </div>
-            <div class="grid__item" tabindex="13">
-              <a class="product__link _link" href="/how_to_work">Как оформить заказ ></a>
-            </div>
-          </div>
+                          </div>
+                          <div class="opt_price">
+                              <div>Цена со скидкой: </div>
+                              <div>
+                                  <span class="price__value">{{
+                                cartItemData.price_with_tax }}</span>BYN
+                                  <span>/шт</span>
+                              </div>
+
+                          </div>
+                      </div>
+
+
+
+
+                  </div>
+                  <div class="product__label">*Все цены указаны с учетом НДС.</div>
+                  <div class="product__button flex-center">
+                      <div v-if="quantity !== 0" class="btn empty_black" @click.stop="onOperationWithCartItem(cartItemData, 'set')">В корзине</div>
+                      <div v-if="quantity === 0 && cartItemData.status !== 'O'" class="btn black" @click.stop="onOperationWithCartItem(cartItemData, 'set')">В корзину</div>
+                      <div v-if="quantity === 0 && cartItemData.status === 'O'" class="btn empty_black popup-btn" @click.stop="onCreatePopUp(true, cartItemData.id)">Узнать о поступлении</div>
+
+                      <div
+                              @click.stop="onWishClick()"
+                              :class="[isWish === false ? 'product__favorite icon-favorite' : 'product__favorite icon-favorite-choosed']"
+
+                      >
+                      </div>
+                  </div>
+                  <a class="product__link _link" href="/how_to_work">Как оформить заказ ></a>
+
+
+
+            </div> <!-- product__info -->
+          </div>  <!-- product__box -->
 
 
         </div>
@@ -385,132 +373,42 @@
 
 <style scoped lang="scss">
 
-.grid {
-  display: grid;
-  align-items: start;
-  justify-items: start;
-  //grid-gap: 1.5vw;
-  //min-height: 100vh;
-  //padding: 1.5vw;
-  transition: 2s;
-  &__item{
-    padding: 5px 0;
-  }
-}
-
-
-.grid {
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows:auto;
-
-  grid-template-areas:
-      "photo   photo"
-      "status   status"
-      "title   title"
-      "arcticle arcticle"
-      "add_price wishlist"
-      "count count"
-      "price cart_button"
-      "link link";
-}
-.grid .item:nth-child(6){
-  display: none;
-}
-
-@media screen and (min-width: $md2+px) {
-  .grid {
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: auto;
-
-    grid-template-areas:
-        "breadcrumb breadcrumb breadcrumb breadcrumb breadcrumb "
-        "title title  title title title"
-        "photo photo photo arcticle arcticle"
-        "photo photo photo status status"
-        "photo photo photo add_price add_price"
-        "photo photo photo count count"
-        "photo photo photo price price"
-        "photo photo photo cart_button wishlist"
-        "big1 big1 big1 link link";
-  }
-  .grid .item:nth-child(6){
-    display: none;
-  }
-}
-
-@media screen and (min-width: $md1+px) {
-  .grid {
-    grid-template-columns: repeat(7, 1fr);
-    grid-template-rows: auto;
-    gap: 5px;
-    grid-template-areas:
-      "breadcrumb breadcrumb  breadcrumb breadcrumb breadcrumb share  share"
-      "photo photo photo title title title title"
-      "photo photo photo arcticle arcticle arcticle arcticle"
-      "photo photo photo status status status status"
-      "photo photo photo count count count count"
-      "photo photo photo price price add_price add_price"
-      "big1 big1 big1 cart_button cart_button wishlist wishlist"
-      "big1 big1 big1 link link link link";
-  }
-}
-
-.grid__item:nth-child(1)  {
-  grid-area: photo;
-  grid-row: 1/9;
-  align-self: center;
-  justify-self: center;
-  @media (max-width: $md2+px){
-    grid-row: 1;
-  }
-}
-.grid__item:nth-child(2)  { grid-area: big1; }
-.grid__item:nth-child(3)  { grid-area: title; }
-.grid__item:nth-child(4)  { grid-area: arcticle; }
-.grid__item:nth-child(5)  { grid-area: breadcrumb; }
-.grid__item:nth-child(6)  { grid-area: share; }
-.grid__item:nth-child(7)  { grid-area: status; }
-.grid__item:nth-child(8)  { grid-area: price; }
-.grid__item:nth-child(9)  {
-  grid-area: wishlist;
-  align-self: center;
-  justify-content: start;
-  padding-left: 20px;
-;
-}
-.grid__item:nth-child(10) { grid-area: add_price; }
-.grid__item:nth-child(11) {
-  grid-area: cart_button;
-  align-self: center;
-  @media (max-width: $md2+px){
-
-  }
-}
-.grid__item:nth-child(12) { grid-area: count; }
-.grid__item:nth-child(13) { grid-area: link; }
-
-
-
-
 .product {
 
-  &__wrapper{
-
-
-  }
-  &__content{
-
-  }
+  &__wrapper{}
+  &__content{}
 
   &__body{
     .if_status_on_the_way{
       font-size: 12px;
       margin-left: 25px;
     }
+  }
+  &__box{
+    display: flex;
+    margin-bottom: 40px;
+    @media (max-width: $md3+px) {
+      flex-direction: column;
+      font-size: 10px;
+      margin-bottom: 10px;
+    }
 
   }
+  &__main-img{
+    flex-basis: 40%;
+  }
+  &__info{
+    flex-basis: 60%;
+    padding: 0 0 0 10px;
+    position: relative;
+    @media (max-width: $md3+px) {
+      width: 100%;
+      padding: 0 0 0 0;
+      display: flex;
+      flex-direction: column;
+    }
+  }
   &__status-row{
-
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -547,53 +445,63 @@
       &:nth-child(1){
         display: flex;
         flex-direction: column;
-
       }
 
-      &:nth-child(2){
-
+      &:nth-child(2){}
+    }
+  }
+  &__label{
+      font-size: 12px;
+      line-height: 20px;
+      text-align: right;
+      margin-bottom: 20px;
+      @media (max-width: $md3+px) {
+        font-size: 8px;
+        margin-bottom: 5px;
       }
-
-
-    }
-  }
-  &__main-img{
-
-    flex-basis: 80%;
-    img{
-      width: 100%;
-    }
-
-
-  }
-  &__add-img{
-
-    flex-basis: 20%;
-    img{
-      width: 100%;
-    }
-
-
   }
   &__btn{
     background: #423E48;
     font-weight: 400;
     padding: 13px 40px;
 
+  }
+  &__button{
+    margin-bottom: 20px;
+    @media (max-width: $md3+px) {
+      position: absolute;
+      right: 0;
+      bottom: 52px;
+      margin-bottom: 0;
+      .btn{
+        min-width: auto;
+      }
+
+    }
 
   }
+
+
   &__favorite{
     cursor: pointer;
+    margin-left: 20px;
+    @media (max-width: $md3+px) {
+      position: absolute;
+      right: 39px;
+      bottom: 70px;
+      font-size: 20px;
+    }
 
   }
   &__link{
-    font-size: 14px;
     opacity: 0.7;
+    @media (max-width: $md3+px) {
+      font-size: 10px;
+    }
   }
   &__action{
     margin:30px 0 20px 0;
   }
-
 }
 
 .desc-product{
@@ -604,13 +512,17 @@
     font-size: 20px;
     line-height: 28px;
     color: #423E48;
+    margin-bottom: 8px;
+    @media (max-width: $md3+px) {
+      font-size: 16px;
+    }
   }
 
   &__article{
-
-
+    margin-bottom: 16px;
      @media (max-width: $md2+px) {
-       margin-bottom: 20px;
+       margin-bottom: 5px;
+       font-size: 10px;
     }
 
   }
@@ -619,11 +531,18 @@
     &:before{
       margin-right: 10px;
     }
+    @media (max-width: $md3+px) {
+      order: -1;
+      font-size: 10px;
+      margin-top: 10px;
+
+    }
   }
 
   &__count{
+    margin: 30px 0;
     @media (max-width: $md2+px) {
-      margin: 20px 0;
+      margin: 10px 0;
     }
       span{
         cursor: pointer;
@@ -631,6 +550,32 @@
           margin-right: 10px;
         }
       }
+
+  }
+  &__price{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #F9F9F9;
+    padding: 22px 18px 20px 18px;
+    font-size: 14px;
+    font-weight: 300;
+    @media (max-width: $md3+px) {
+      flex-direction: column-reverse;
+      align-items: stretch;
+      padding: 22px 18px 0 0;
+      font-size: 10px;
+      background: none;
+      .price__left{
+        margin-top: 20px;
+        background: #F9F9F9;
+        padding: 10px 0 5px 10px;
+        margin: 20px -10px 0 -10px;
+
+      }
+
+    }
+
 
   }
 
@@ -644,23 +589,9 @@
     margin: 0 10px;
   }
 
-  &__price{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background: #F9F9F9;
-    padding: 22px 18px 20px 18px;
-    .retail_price{
-
-    }
-
-  }
-
-
   &__favorive{
     cursor: pointer;
     &:hover{
-      background: red;
     }
   }
 
@@ -672,14 +603,18 @@
   font-weight: 500;
   font-size: 24px;
   line-height: 36px;
+  &_item{
+    font-weight: 300;
+  }
   @media (max-width: $md3 + px){
-
+    font-size: 14px;
   }
   .old_price {
     text-decoration-line: line-through;
     opacity: 0.4;
     white-space: nowrap;
     font-weight: 300;
+    font-size: 18px;
   }
   span{
     margin-right: 10px;
@@ -693,6 +628,9 @@
   display: flex;
   align-items: center;
   justify-content: space-between;
+  @media (max-width: $md3+px) {
+    justify-content: flex-start;
+  }
   .price__value{
     margin: 0 5px;
     font-weight: 600;
@@ -764,7 +702,7 @@
     transition: 0.3s;
     font-size: 17px;
     @media (max-width: $md3+px) {
-        font-size: 14px;
+        font-size: 12px;
         padding: 10px 10px;
     }
   }
