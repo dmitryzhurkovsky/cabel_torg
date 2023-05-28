19bdd32ed5618c253c2df43f9b2d2be302bd6ffd
dmitryzhurkovsky <dmitryzhurkovsky@gmail.com>
2023-05-28 17:48:50 +0300

    Fix script for backups

--- backend/scripts/backup.sh
+++ backend/scripts/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-date=date=$(date '+%Y-%m-%d')
+date=$(date '+%Y-%m-%d')
 source backend/env.prod
 
-echo "Start logging process in $date\n" > /var/log/cabel_torg_backup.log
+echo "Start logging process in $date" > /var/log/cabel_torg_backup.log
 
 BACKUP_FILE="${date}_backup.sql"
 
 pg_dump -U $DB_USERNAME -W $DB_NAME > $BACKUP_FILE
-echo "Database has been done on ${date}" >> /var/log/cabel_torg_backup.log
+echo "Database backup has been created on ${date}" >> /var/log/cabel_torg_backup.log
 
 cp -r /root/cabel_torg/data/* /root/dumped_data/
-echo "Data was copied ${date}" >> /var/log/cabel_torg_backup.log
+echo "Data was copied on ${date}" >> /var/log/cabel_torg_backup.log
