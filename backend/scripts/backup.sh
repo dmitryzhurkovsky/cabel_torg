3ab42332fac63e622a82c0e092adf1d4d8dda69b
dmitryzhurkovsky <dmitryzhurkovsky@gmail.com>
2023-06-06 10:56:55 +0300

    Change backup script

--- backend/scripts/backup.sh
+++ backend/scripts/backup.sh
@@ -4,6 +4,7 @@ DATA_DIR=/data
 LOGS_DIR=$DATA_DIR/logs
 BACKUPS_DIR=/backups # It's a folder for all files that should be kept, including site_media, ccertbot, parser's files.
 DOCKER_BACKUPS_DIR=/backups  # It's used for indicating a folder in a ocker container. Should be change both in docker-compose file and here.
+DOCKER_COMPOSE_FILE=$PROJECT_DIR/docker-compose.yaml
 LOG_FILE=$LOGS_DIR/cabel_torg_backup_process.log
 
 # Create the backup and logs directories.
@@ -17,8 +18,8 @@ source $PROJECT_DIR/backend/.env
 date=$(date "+%Y-%m-%d")
 
 #echo "Start logging process in $date" > $LOG_FILE
-PGPASSWORD=$POSTGRES_PASSWORD docker-compose exec -T db mkdir -p $DOCKER_BACKUPS_DIR
-PGPASSWORD=$POSTGRES_PASSWORD docker-compose exec -T db pg_dump -U $POSTGRES_USER -d $POSTGRES_DB -f "$DOCKER_BACKUPS_DIR/${date}_backup.sql"
+PGPASSWORD=$POSTGRES_PASSWORD docker-compose -f $DOCKER_COMPOSE_FILE exec -T db mkdir -p $DOCKER_BACKUPS_DIR
+PGPASSWORD=$POSTGRES_PASSWORD docker-compose -f $DOCKER_COMPOSE_FILE exec -T db pg_dump -U $POSTGRES_USER -d $POSTGRES_DB -f "$DOCKER_BACKUPS_DIR/${date}_backup.sql"
 echo "Database backup has been created on ${date}" >> $LOG_FILE
 
 rsync -a --stats --exclude="backups" --exclude="logs" "$DATA_DIR/" "$BACKUPS_DIR/"
