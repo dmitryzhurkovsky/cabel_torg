4e3083a3dc3d004ad90273874b88471f69696342
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2023-06-08 16:32:23 +0200

    Change color and size recomendation nav

--- ssr/pages/category/[id].vue
+++ ssr/pages/category/[id].vue
@@ -28,7 +28,7 @@
               </div>
               <div v-if = "LastCategory?.length && isMobileVersion">
                 <swiper
-                  :slides-per-view="1.7"
+                  :slides-per-view="2.7"
                   :space-between="12"
                   :speed="500"
                   :autoplay="{
@@ -434,6 +434,10 @@
       a{
         color: #423E48;
       }
+      @media (max-width: $md2+px) {
+        background: #dedede;
+        opacity: 0.6;
+      }
 
       &:hover{
         box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.08);
@@ -550,6 +554,10 @@
   //max-width: 180px;
   transition: all 0.5s ease;
   cursor: pointer;
+  @media (max-width: $md2+px) {
+    background: #dedede;
+    opacity: 0.6;
+  }
 
 }
 .recomendation__nav .active {
