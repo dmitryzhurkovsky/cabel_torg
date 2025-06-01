5f5a47d1739ffd14801e03349659eb00adf2d331
kiselmen <kiselmen@rambler.rum>
2025-06-01 13:24:01 +0300

    fix: canonicl middlware, add query parameters

--- ssr/middleware/canonical.global.js
+++ ssr/middleware/canonical.global.js
@@ -1,16 +1,27 @@
 export default defineNuxtRouteMiddleware((to, from) => {
 
-  console.log('canonical to: ', to.fullPath, ' from: ', from.fullPath);
-  let rebuildedTarget = to.fullPath.toLowerCase();
-  if (to.fullPath) {
+  // console.log('canonical to: ', to.path, ' from: ', from.path);
+  let rebuildedTarget = to.path.toLowerCase();
+
+  const originalString = to.fullPath;
+  const indexOfQuestionMark = originalString.indexOf('?');
+  
+  const paramsStr = indexOfQuestionMark !== -1 
+      ? originalString.substring(indexOfQuestionMark) 
+      : '';
+
+  // console.log('query: ', paramsStr);
+  
+  if (to.path) {
     if (rebuildedTarget.startsWith('www.')) {
       rebuildedTarget = rebuildedTarget.slice(4);
     }
     rebuildedTarget = rebuildedTarget.replace(/\/{2,}/g, '/');
     if (rebuildedTarget.endsWith('/')) rebuildedTarget = rebuildedTarget.slice(0, -1)
   }
-  if (to.fullPath !== rebuildedTarget) {
-    console.log('Redirect is needed to: ', rebuildedTarget);
+  if (to.path !== rebuildedTarget) {
+    if (paramsStr.length) rebuildedTarget = rebuildedTarget + paramsStr;
+    // console.log('Redirect is needed to: ', rebuildedTarget);
     return navigateTo(rebuildedTarget, { redirectCode: 301 });
   }
 });
